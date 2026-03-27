57f20c49e34f704322d3b5c058c0c4ff3c9d7526
bain66888 <bain66888@gmail.com>
2026-03-27 14:50:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,21 +4,25 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci.main.lang=en/luci.main.lang=zh_cn/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
-# 下载 AdGuardHome (已经稳了，保留)
+# 2. 提前创建多级目录 (确保 tar 有地方解压)
+mkdir -p files/usr/bin/AdGuardHome
+mkdir -p files/etc/openclash/core
+
+# 3. 插件核心下载 (使用静态 CDN 或固定链接，避开 API 限制)
 echo "Downloading AdGuardHome..."
-wget -t 5 -T 15 "https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_arm64.tar.gz" -O- | tar xz -C files/usr/bin/AdGuardHome --strip-components=2
+wget -t 5 -T 15 "https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_arm64.tar.gz" -O agh.tar.gz
+tar -xzf agh.tar.gz -C files/usr/bin/AdGuardHome --strip-components=2 && rm agh.tar.gz
 
-# 下载 OpenClash Dev 核心 (修正路径)
 echo "Downloading OpenClash Dev Core..."
-wget -t 5 -T 15 "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz" -O- | tar xz -C files/etc/openclash/core
+# 换成这个项目专门存放 core 的 master 路径
+wget -t 5 -T 15 "https://raw.githubusercontent.com/vernesong/OpenClash/master/resources/overrides/clash-linux-arm64.tar.gz" -O dev.tar.gz
+tar -xzf dev.tar.gz -C files/etc/openclash/core && rm dev.tar.gz
 
-# 下载 OpenClash Meta 核心 (修正文件名为 v8a-v 开头的格式)
 echo "Downloading OpenClash Meta Core..."
-# 针对 mihomo 仓库，我们直接抓取最新的 release 链接
-META_URL=$(curl -s https://api.github.com/repos/MetaCubeX/mihomo/releases/latest | grep "browser_download_url" | grep "linux-arm64-v8a" | grep "tar.gz" | head -n 1 | cut -d '"' -f 4)
-wget -t 5 -T 15 "$META_URL" -O- | tar xz -C files/etc/openclash/core
-
-# 重命名 Meta 核心以匹配 OpenClash
+# 暂时使用固定版本链接以确保 100% 成功，后续进系统再更新即可
+wget -t 5 -T 15 "https://github.com/MetaCubeX/mihomo/releases/download/v1.19.1/mihomo-linux-arm64-v8a.tar.gz" -O meta.tar.gz
+tar -xzf meta.tar.gz -C files/etc/openclash/core && rm meta.tar.gz
 mv files/etc/openclash/core/mihomo files/etc/openclash/core/clash_meta 2>/dev/null
 
+# 赋权
 chmod +x files/usr/bin/AdGuardHome/AdGuardHome files/etc/openclash/core/clash* 2>/dev/null
