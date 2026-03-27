4a8186b6f56946f01d799bc3a6518ed3d53fff83
bain66888 <bain66888@gmail.com>
2026-03-27 16:11:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,8 @@ echo "Downloading AdGuardHome..."
 wget -t 5 -T 15 "https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_arm64.tar.gz" -O agh.tar.gz && \
 tar -xzf agh.tar.gz -C files/usr/bin/AdGuardHome --strip-components=2 && rm agh.tar.gz || echo "AGH download failed"
 
+# 暴力修复：直接在驱动头文件中插入缺少的定义
+sed -i '40i #define HIT_BIND_FORCE_TO_CPU 0x3\n#define MTK_FE_START_RESET 0x10\n#define MTK_FE_RESET_DONE 0x11\n#define MTK_FE_RESET_NAT_DONE 0x12\n#define MTK_WIFI_CHIP_ONLINE 0x13\n#define MTK_WIFI_CHIP_OFFLINE 0x14\n#define MTK_WIFI_RESET_DONE 0x15' target/linux/mediatek/files-6.6/drivers/net/ethernet/mediatek/mtk_eth_soc.h
 # 3. 赋权并退出
 chmod -R +x files/usr/bin/AdGuardHome 2>/dev/null
 exit 0
